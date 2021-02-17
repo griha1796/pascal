c35305c6e83ff3e5850ac613eb05e4b72e37d384
griha1796 <69796095+griha1796@users.noreply.github.com>
2021-02-17 23:29:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+  el: "body",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x3fff60,
+  backgroundColor: 0x151e3c,
+  points: 12.00,
+  maxDistance: 23.00
+})
